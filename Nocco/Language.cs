f85b89e3147c02ae2eaeec79ba8ab7a4c0c2def8
t.bleuse@gmail.com <@Oueue2$>
2015-05-13 19:46:42 +0200

    Change langage definition

--- Nocco/Language.cs
+++ Nocco/Language.cs
@@ -13,5 +13,25 @@ namespace Nocco
 		public Regex CommentFilter { get { return new Regex(@"(^#![/]|^\s*#\{)"); } }
 		public IDictionary<string, string> MarkdownMaps;
 		public IList<string> Ignores;
+
+        // We want to consider C# regions as comment symbol
+        public List<string> Symbols { get; set; }
+
+        public List<Regex> CommentMatchers
+        {
+            get
+            {
+                List<Regex> res = new List<Regex>();
+                foreach (string symbol in Symbols)
+                {
+                    res.Add(new Regex(@"^\s*" + symbol + @"\s?"));
+                }
+                return res;
+            }
+        }
+
+        // We don't want to see includes in the documentation
+        public List<string> IgnoreOnStart { get; set; }
+        
 	}
 }
